df6e9cbf1e6526ede64fc8a77ffb82966ded23c3
Maurot11 <mauro.co.uk@hotmail.com>
2024-02-01 17:40:31 +0000

    Send user inputs to DB and displays on webpage

--- client/main.js
+++ client/main.js
@@ -1,13 +1,47 @@
 const form = document.querySelector('form')
+const messageBoard = document.getElementById('messageBoard')
+const serverURL = 'http://localhost:9090'
 
 function handleMessage(e) {
   e.preventDefault()
   console.log('form submitted!')
 
   const formData = new FormData(form)
-  const message = Object.fromEntries(formData)
+  const messages = Object.fromEntries(formData)
 
-  console.log(message)
+  fetch(`${serverURL}/guest`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({messages})
+  })
 }
+
+async function fetchMessages() {
+  const messages = await fetch(`${serverURL}/guest`)
+  let result = await messages.json()
+  return result
+}
+
+async function displayMessages() {
+  let messages = await fetchMessages()
+
+  messageBoard.innerHTML = '';
+  messages.forEach(message => {
+    let h3Txt = document.createElement('h3')
+    let pTxt = document.createElement('p')
+    
+    h3Txt.textContent = message.name
+    h3Txt.setAttribute('class', 'name')
+    pTxt.textContent = message.message
+    pTxt.setAttribute('class', 'message')
+
+    messageBoard.appendChild(h3Txt)
+    messageBoard.appendChild(pTxt)
+  })
+}
+
+displayMessages()
 
 form.addEventListener('submit', handleMessage)
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import Database from "sqlite3";
+import Database from "better-sqlite3";
 
 const app = express()
-// const db = new Database('database.db')
+const db = new Database('database.db')
 const PORT = '9090'
 
 app.use(express.json())
@@ -12,6 +12,29 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send({message: 'HELLO'})
     res.status(200)
+})
+
+app.get('/guest', (req, res) => {
+    try {
+        let messages = db.prepare(`SELECT * FROM guests`).all()
+        res.status(200).json(messages)
+    } catch (err) {
+        res.status(500).json({error: err})
+    }
+})
+
+app.post('/guest', (req, res) => {
+    try {
+        const name = req.body.messages.name
+        const message = req.body.messages.message
+        // console.log(req.body.messages.name)
+
+        const newMessage = db.prepare(`INSERT INTO guests (name, message) VALUES (?, ?)`).run(name, message)
+        console.log(newMessage)
+        res.status(200).json(newMessage)
+    } catch (err) {
+        res.status(500).json({error: err})
+    }
 })
 
 app.get('/guest', (req, res) => {
